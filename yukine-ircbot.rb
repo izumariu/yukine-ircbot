d8cc796ef0bbf529c6e1e2f1f6f85ea3831a70c2
sesshomariu <sesshotv@t-online.de>
2016-05-30 15:02:02 +0200

    Added „efa“-mode

--- yukine-ircbot.rb
+++ yukine-ircbot.rb
@@ -1,20 +1,20 @@
 #!/usr/bin/ruby
 require 'cinch' #gem install cinch
 
-nice_users = [
+$nice_users = [
 	"sessho",
 	"moeterfan",
 	"nola",
 	"zuckerwatte",
-	"zucker",
 	"rai",
-	"raipat"
+	"raipat",
+	"efa"
 ]
-
-
 
 $NICE = false
 $SH = false
+
+
 
 ircBot = Cinch::Bot.new do
 	configure do |c|
@@ -23,6 +23,111 @@ ircBot = Cinch::Bot.new do
 		c.server = "irc.moep.net"
 		c.channels = ["#klartraum"]
 	end
+	
+	on :message, /\s{0}\S{1,}\+\+.{0}/m do |m|
+		if m.params[1].split("++")[0].downcase != "yukine"
+			$nice_users.each do |user|
+				if m.params[1].split("++")[0].downcase == user.downcase
+					$NICE = true
+					debug "#{m.params[1].split("++")[0].downcase} matched #{user.downcase}!"
+					break
+				else
+					$NICE = false
+					debug "#{m.params[1].split("++")[0].downcase} did not match #{user.downcase}."
+				end
+			end
+			sleep 5
+			if $NICE == true
+				n_msg = [
+					"#{m.params[1].split("++")[0]} hat dieses Lob verdient!",
+					#"Können wir mal kurz reden, #{m.params[1].split("++")[0]}? Bitte, ich will Zeit mit dir verbringen.... wir sehen uns so selten...",
+					"#{m.params[1].split("++")[0]}: Stimmt, da hat sie Recht... wenn es nichts perverses ist...",
+					"#{m.params[1].split("++")[0]}: Definitiv!",
+					"#{m.params[1].split("++")[0]}: Muss ich efa Recht geben!"
+				]
+				m.reply(n_msg[Random.rand(n_msg.length)])
+			else
+				f_msg = [
+					"Scher dich dahin, wo der Pfeffer wächst, #{m.params[1].split("++")[0]} -.-#",
+					#"Ihr versteht mich alle nicht!! WAS IST EUER PROBLEM?!?",
+					"#{m.params[1].split("++")[0]} hat sich das nicht verdient, so gelobt zu werden...",
+					#"Anstatt mal Yato zu unterstützen müsst ihr alle hier herumlungern....",
+					"Boah, #{m.params[1].split("++")[0]}? Das glaubst du ja wohl selber nicht, efa!",
+					"#{m.params[1].split("++")[0]}: Sei doch so nett und HALT DIE KLAPPE.",
+					"#{m.params[1].split("++")[0]}: Ist klar. Du hast bisher noch NICHTS in diesem IRC hier richtig gemacht. Und in deinem Leben wahrscheinlich auch nicht."
+				]
+				m.reply(f_msg[Random.rand(f_msg.length)])
+			end
+		else
+			sleep 5
+			m.reply("Danke, efa... *erröt*")
+		end
+		$NICE = false
+	end
+	
+	on :message, /\s{0}\S{1,}\-\-.{0}/m do |m|
+		if m.params[1].split("--")[0].downcase != "yukine"
+			$nice_users.each do |user|
+				if m.params[1].split("--")[0].downcase == user.downcase
+					$NICE = true
+					debug "#{m.params[1].split("--")[0].downcase} matched #{user.downcase}!"
+					break
+				else
+					$NICE = false
+					debug "#{m.params[1].split("--")[0].downcase} did not match #{user.downcase}."
+				end
+			end
+			sleep 5
+			if $NICE == true
+				n_msg = [
+					"#{m.params[1].split("--")[0]} hat das nicht verdient! :(",
+					#"Können wir mal kurz reden, #{m.params[1].split("--")[0]}? Bitte, ich will Zeit mit dir verbringen.... wir sehen uns so selten...",
+					"#{m.params[1].split("--")[0]} beleidigen! Die schlechteste Idee, auf die du je gekommen bist, efa!",
+					"#{m.params[1].split("--")[0]}: Hör nicht auf efa, sie meint es nicht so...",
+					"#{m.params[1].split("--")[0]}: efa ist eine alte Schrapnelle, nimm das nicht ernst."
+				]
+				m.reply(n_msg[Random.rand(n_msg.length)])
+			else
+				f_msg = [
+					"Scher dich dahin, wo der Pfeffer wächst, #{m.params[1].split("--")[0]} -.-#",
+					#"Ihr versteht mich alle nicht!! WAS IST EUER PROBLEM?!?",
+					"#{m.params[1].split("--")[0]} hat sich das verdient, so gedemütigt zu werden...",
+					#"Anstatt mal Yato zu unterstützen müsst ihr alle hier herumlungern....",
+					"Boah, #{m.params[1].split("--")[0]}! Einer muss es ja sagen, efa!",
+					"#{m.params[1].split("--")[0]}: Genau. Sei doch so nett und HALT DIE KLAPPE.",
+					"#{m.params[1].split("--")[0]}: Siehst du? Du hast bisher noch NICHTS in diesem IRC hier richtig gemacht. Und in deinem Leben wahrscheinlich auch nicht."
+				]
+				m.reply(f_msg[Random.rand(f_msg.length)])
+			end
+		else
+			sleep 5
+			m.reply("efa!! WARUM NUR??? *efa eine knall*")
+		end
+		$NICE = false
+	end
+	
+#	on :message, "!nice-on" do |m|
+#		$being_nice = Thread.new {
+#			loop do
+#				g_msgs = [
+#					" ist übrigens eine ganz tolle Person! :)",
+#					" macht seine/ihre Sache gut.",
+#					" ist mir einfach total sympathisch!",
+#					"... hat dir heute schon mal jemand gesagt, dass du echt gut aussiehst?",
+#					" mag mich hoffentlich. Ich mag die Person nämlich auch sehr...",
+#					", wenn du nicht wärst, was wäre dann? Also ich definitiv nicht.",
+#					", ach, wenn du wüsstest, was du für ein toller Mensch bist... :)",
+#					", kannst du bitte Yato 5 Yen spenden? Das wär so nett von dir.... ;)"
+#				]
+#				m.reply($nice_users[Random.rand($nice_users.length)] << g_msgs[Random.rand(g_msgs.length)])
+#				sleep 5
+#			end
+#		}
+#	end
+#	
+#	on :message, "!nice-off" do |m|
+#		Thread.kill($being_nice)
+#	end
 	
 	on :message, "!sh-off" do |m|
 		if m.user.nick == "moeterfan"
@@ -86,7 +191,7 @@ ircBot = Cinch::Bot.new do
 	end
 	
 	on :message, "!iloveyuki" do |m|
-		nice_users.each do |user|
+		$nice_users.each do |user|
 			if m.user.nick.downcase == user.downcase
 				$NICE = true
 				debug "#{m.user.nick.downcase} matched #{user.downcase}!"
@@ -114,44 +219,48 @@ ircBot = Cinch::Bot.new do
 	end
 	
 	on :message, /yukine/ do |m|
-		nice_users.each do |user|
-			if m.user.nick.downcase == user.downcase
-				$NICE = true
-				debug "#{m.user.nick.downcase} matched #{user.downcase}!"
-				break
+		if m.user.nick != "efa" and (m.params[1][-1] != "-" and m.params[1][-2] != "-") and (m.params[1][-1] != "+" and m.params[1][-2] != "+")
+			$nice_users.each do |user|
+				if m.user.nick.downcase == user.downcase
+					$NICE = true
+					debug "#{m.user.nick.downcase} matched #{user.downcase}!"
+					break
+				else
+					$NICE = false
+					debug "#{m.user.nick.downcase} did not match #{user.downcase}."
+				end
+			end
+			
+			debug "#{m}"
+			
+			if $NICE == true
+			
+				n_msg = [
+					"#{m.user.nick} versteht mich, der Rest außer gewissen anderen Personen nicht ;__;",
+					#"Können wir mal kurz reden, #{m.user.nick}? Bitte, ich will Zeit mit dir verbringen.... wir sehen uns so selten...",
+					"#{m.user.nick} ist echt wunderbar.... :)",
+					"#{m.user.nick} unterstützt Yato! Nehmt euch mal ein Beispiel! -.-",
+					"#{m.user.nick}: *erröt*"
+				]
+				m.reply(n_msg[Random.rand(n_msg.length)])
+				
 			else
-				$NICE = false
-				debug "#{m.user.nick.downcase} did not match #{user.downcase}."
+			
+				f_msg = [
+					"Ach, halt doch die Klappe, #{m.user.nick} -.-#",
+					#"Ihr versteht mich alle nicht!! WAS IST EUER PROBLEM?!?",
+					"War ja klar. Ich bin wieder der Dumme, nicht wahr, #{m.user.nick}? Wie immer. Ich hasse es.",
+					"Anstatt mal Yato zu unterstützen müsst ihr alle hier herumlungern....",
+					"Boah, #{m.user.nick} hat sich wieder mal nicht gewaschen...",
+					"#{m.user.nick}: Natürlich, ich hab ja keine anderen Probleme, du Nervensäge -.-",
+					"#{m.user.nick}: Du liebst es, auf mir herumzuhacken, nicht wahr? -.-#"
+				]
+				m.reply(f_msg[Random.rand(f_msg.length)])
+					
 			end
 		end
-		if $NICE == true
-		
-			n_msg = [
-				"#{m.user.nick} versteht mich, der Rest außer gewissen anderen Personen nicht ;__;",
-				#"Können wir mal kurz reden, #{m.user.nick}? Bitte, ich will Zeit mit dir verbringen.... wir sehen uns so selten...",
-				"#{m.user.nick} ist echt wunderbar.... :)",
-				"#{m.user.nick} unterstützt Yato! Nehmt euch mal ein Beispiel! -.-",
-				"#{m.user.nick}: *erröt*"
-			]
-			m.reply(n_msg[Random.rand(n_msg.length)])
-			
-		else
-		
-			f_msg = [
-				"Ach, halt doch die Klappe, #{m.user.nick} -.-#",
-				#"Ihr versteht mich alle nicht!! WAS IST EUER PROBLEM?!?",
-				"War ja klar. Ich bin wieder der Dumme, nicht wahr, #{m.user.nick}? Wie immer. Ich hasse es.",
-				"Anstatt mal Yato zu unterstützen müsst ihr alle hier herumlungern....",
-				"Boah, #{m.user.nick} hat sich wieder mal nicht gewaschen...",
-				"#{m.user.nick}: Natürlich, ich hab ja keine anderen Probleme, du Nervensäge -.-",
-				"#{m.user.nick}: Du liebst es, auf mir herumzuhacken, nicht wahr? -.-#"
-			]
-			m.reply(f_msg[Random.rand(f_msg.length)])
-				
-		$NICE = false
-		
-		end
 	end
+	$NICE = false
 end
 
 ircBot.start
